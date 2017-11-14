ca009c965e32d8fb2e4899140e1f954ee9af02ae
Jay Hodgson <jay.hodgson@sagebase.org>
2017-11-13 17:24:08 -0800

    include dist

--- service.py
+++ service.py
@@ -1,8 +1,12 @@
 # -*- coding: utf-8 -*-
+# used python-lambda (https://github.com/nficano/python-lambda) to set up environment
 from urllib import urlopen
 from nbconvert import HTMLExporter
 import nbformat
 
+# Converts ipynb file referenced by the 'file' param (in the post request) and returns html.
+# Ideally we would convert to pdf.  Tried using command line call (via subprocess), but it depends on TeX being installed (which is a heavy ask for the lambda environment).
+# https://nbconvert.readthedocs.io/en/latest/install.html#installing-tex
 def handler(event, context):
     response = urlopen(event.get('file')).read().decode()
     notebook = nbformat.reads(response, as_version=4)
