ca725496e86f692d3c6dc67e3851a2218d0ed9af
Jay Hodgson <jay.hodgson@sagebase.org>
2017-11-14 07:55:53 -0800

    set a max file size

--- service.py
+++ service.py
@@ -8,7 +8,14 @@ import nbformat
 # Ideally we would convert to pdf.  Tried using command line call (via subprocess), but it depends on TeX being installed (which is a heavy ask for the lambda environment).
 # https://nbconvert.readthedocs.io/en/latest/install.html#installing-tex
 def handler(event, context):
-    response = urlopen(event.get('file')).read().decode()
+    site = urlopen(event.get('file'))
+    meta = site.info()
+    contentLength = int(meta.get_all('Content-Length')[0])
+    # print ("Content-Length:", contentLength)
+    if contentLength > 15728640:
+        raise ValueError('Notebook file is too large. It must be less than 15MB: ' + event.get('file'))
+    response = site.read().decode()
+    
     notebook = nbformat.reads(response, as_version=4)
     html_exporter = HTMLExporter()
     html_exporter.template_file = 'basic'
